2ee4a7958216de5b2b234c82cf31399e9aa2f0e1
Bibhudendu Behera <bibhu342@gmail.com>
2025-11-09 22:50:08 +0530

    part II: word cloud + timeline (monthly & yearly) + screenshots

---
+++ gline.js
@@ -0,0 +1,4 @@
+gline = [ ['Year','umich.edu','indiana.edu','ufp.pt','ucdavis.edu','uct.ac.za','berkeley.edu','columbia.edu','gmail.com','mac.com','virginia.edu'],
+['2005',56,12,10,10,14,12,13,10,12,9],
+['2006',75,25,26,21,17,17,14,13,10,13]
+];
